29e3a38ca04f2a2f731bd6d4c933237c0286c20d
Alexander Ryzhikov <alexander.ryzhikov@relex.fi>
2016-08-09 11:29:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+### Helps you to use Native JS methods where possible
+
+works only with ES6 code
+
+rules are taken fron
+
+`.eslintrc`
+```
+{
+  "plugins": [
+    "native-over-lodash"
+  ]
+}
+```
---
+++ tests/index.js
@@ -0,0 +1,47 @@
+const rules = require('../lib/rules');
+const RuleTester = require('eslint').RuleTester;
+const kebabCase = require('kebab-case');
+
+const ruleTester = new RuleTester({
+    parser: 'babel-eslint'
+});
+const rule = rule => rules[`lodash/${kebabCase(rule)}`];
+
+ruleTester.run('concat', rule('concat'), {
+  valid: [
+    'array.concat(2, [3], [[4]])'
+  ],
+  invalid: [{
+    code: 'import concat from "lodash/concat"',
+    errors: ['Consider using the native Array.prototype.concat()']
+  }]
+});
+
+ruleTester.run('index import', rule('index'), {
+    valid: [
+        'import merge from "lodash/merge"'
+    ],
+    invalid: [
+        {
+            code: 'import asd from "lodash"',
+            errors: ['Importing the entire lodash library is not permitted, please import the specific functions you need']
+        }
+    ]
+});
+
+
+ruleTester.run('keys', rule('keys'), {
+    valid: [
+        'Object.keys({one: 1, two: 2, three: 3})'
+    ],
+    invalid: [
+        {
+            code: 'import keys from "lodash/keys"; keys({one: 1, two: 2, three: 3})',
+            errors: ['Consider using the native Object.keys()']
+        },
+        {
+            code: 'import  {_keys} from "lodash/keys"; _keys({one: 1, two: 2, three: 3})',
+            errors: ['Consider using the native Object.keys()']
+        }
+    ]
+});
